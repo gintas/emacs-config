bd34037dde6d056192ed023b4606f7baba868eb7
Gintautas Miliauskas <gintautas@miliauskas.lt>
2012-08-30 23:13:08 +0200

    Enable js2-refactor.

--- init.el
+++ init.el
@@ -52,6 +52,10 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+;; js2-refactor
+(autoload 'js2-refactor "js2-refactor" nil t)
+;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-refactor))
+
 ;; Highlight the line under the cursor
 (global-hl-line-mode 1)
 ;;(set-face-background 'hl-line "#fef8d0")  ;; use for light background
